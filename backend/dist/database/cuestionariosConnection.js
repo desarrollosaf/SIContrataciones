f087c9ba15ee48df50fdc8a108fedd54e32dfdf8
MarSGlez <martin.sanchezg@hotmail.com>
2025-11-04 16:18:39 -0600

    cambios cuestionarios js

--- backend/dist/database/cuestionariosConnection.js
+++ backend/dist/database/cuestionariosConnection.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 const sequelizeCuestionarios = new sequelize_1.Sequelize('adminplem_contrataciones', 'docker_contrataciones', 'dMhqFn5t46Dx75ZTQOSa', {
     host: '192.168.36.54',
+    port: 3307,
     dialect: 'mysql',
     define: {
         freezeTableName: true
--- backend/src/database/cuestionariosConnection.ts
+++ backend/src/database/cuestionariosConnection.ts
@@ -2,6 +2,7 @@ import { Sequelize } from "sequelize"
 
 const sequelizeCuestionarios = new Sequelize('adminplem_contrataciones', 'docker_contrataciones', 'dMhqFn5t46Dx75ZTQOSa', {
     host: '192.168.36.54',
+    port: 3307,
     dialect: 'mysql',
     define: {
         freezeTableName: true 
